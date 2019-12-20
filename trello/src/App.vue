4efb68ecaa2f0f6dc5c93c3a4c7fcc861ee89206
fora <afterup@naver.com>
2019-12-20 15:03:35 +0900

    Add vue scss

--- trello/src/App.vue
+++ trello/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <TheHeader />
+    <div class="container">
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  components: {
+    TheHeader: () => import("@/components/common/TheHeader.vue")
   }
+};
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 120rem;
+  margin: 0 6rem auto;
+  background-color: #ffffff;
+
+  min-height: 50rem;
 }
 </style>
---
+++ trello/vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+                    @import "@/lib/styles/main.scss";
+                `
+      }
+    }
+  }
+};
