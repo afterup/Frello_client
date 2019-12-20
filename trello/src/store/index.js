118a927a9b31279a561971dd61146d7fe74e25c9
fora <afterup@naver.com>
2019-12-20 15:09:50 +0900

    Add signup vuex function

--- trello/src/store/index.js
+++ trello/src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    signup({ commit, dispatch }, authData) {
+      axios
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7cBLEt_0og_yaXqTr01GcTveiuh6Mbas",
+          {
+            email: authData.email,
+            username: authData.username,
+            password: authData.password,
+            returnSecureToken: true
+          }
+        )
+        .then(res => {
+          console.log(res);
+        });
+    }
   },
-  modules: {
-  },
+  modules: {}
 });
