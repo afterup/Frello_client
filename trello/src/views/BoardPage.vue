d157a7540ea7bea3f67a29795515e3097bcf4462
fora <uptohai@gmail.com>
2020-01-14 01:01:26 +0900

    Fix: life cycle change beforeRouteEnter

--- trello/src/views/BoardPage.vue
+++ trello/src/views/BoardPage.vue
@@ -1,125 +1,31 @@
 <template>
 	<div class="container">
-		<div class="board">
-			<!-- boards=> board로 변경-->
-			<div class="board__title">
-				Project Trello
-			</div>
-			<div class="board__favorite">
-				<i class="material-icons">
-					star
-				</i>
-			</div>
-		</div>
-		<div class="list-container">
-			<div class="list">
-				<div class="list__title">
-					Test One
-				</div>
-				<div class="list__items">
-					<div class="list__items__item">
-						<div class="list__items__item__title">
-							one
-						</div>
-					</div>
-					<div class="list__items__item">
-						<div class="list__items__item__title">
-							one
-						</div>
-					</div>
-					<div class="list__items__item">
-						<div class="list__items__item__title">
-							one
-						</div>
-					</div>
-
-					<div class="list__items__add">
-						<i class="material-icons">
-							add
-						</i>
-						<div class="list__items__add__text">Add another card</div>
-					</div>
-				</div>
-			</div>
-			<div class="list-container">
-				<div class="list">
-					<div class="list__title">
-						Test One
-					</div>
-					<div class="list__items">
-						<div class="list__items__item">
-							<div class="list__items__item__title">
-								one
-							</div>
-						</div>
-						<div class="list__items__item">
-							<div class="list__items__item__title">
-								one
-							</div>
-						</div>
-						<div class="list__items__item">
-							<div class="list__items__item__title">
-								one
-							</div>
-						</div>
-
-						<div class="list__items__add">
-							<i class="material-icons">
-								add
-							</i>
-							<div class="list__items__add__text">Add another card</div>
-						</div>
-					</div>
-				</div>
-				<input
-					type="text"
-					v-if="showListInput"
-					v-model="listTitle"
-					@keyup.enter="addList"
-				/>
-				<div class="add-list">
-					<i class="material-icons">
-						add
-					</i>
-					<div class="add-list__text" @click="showListInput = true">
-						Add another list
-					</div>
-				</div>
-			</div>
-		</div>
+		<BoardMeta :board="board" :currentUser="currentUser" />
+		<Columns :lists="board.Lists" />
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import store from '@/store';
 
 export default {
 	data() {
-		return {
-			showListInput: false,
-			listTitle: null,
-		};
+		return {};
 	},
-	created() {
-		this.$store.dispatch('FETCH_BOARD', this.$route.params.id);
+	components: {
+		BoardMeta: () => import('@/components/board/BoardMeta.vue'),
+		Columns: () => import('@/components/board/Columns.vue'),
+	},
+	beforeRouteEnter(to, from, next) {
+		store.dispatch('FETCH_BOARD', to.params.id).then(() => {
+			next();
+		});
 	},
 	computed: {
-		...mapGetters(['board']),
+		...mapGetters(['board', 'currentUser']),
 	},
-	methods: {
-		addList() {
-			this.$store
-				.dispatch('PUBLISH_LIST', {
-					title: this.listTitle,
-					board_id: this.$route.params.id,
-					position: 1,
-				})
-				.then(() => {
-					this.showListInput = false;
-					// this.$store.dispatch('FETCH_BOARD', this.$route.params.id);
-				});
-		},
-	},
+	methods: {},
 };
 </script>
 
@@ -130,126 +36,6 @@ export default {
 	background-color: #ffffff;
 
 	min-height: 50rem;
-}
-
-.board {
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	margin-bottom: 10px;
-	height: 50px;
-}
-
-.board__title {
-	font-size: 1.7rem;
-	font-weight: bold;
-	margin-right: 10px;
-}
-
-.board__favorite {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 25px;
-	height: 25px;
-	border: none;
-	background-color: rgba(187, 187, 187, 0.74);
-	border-radius: 2px;
-}
-
-.board__favorite:hover {
-	cursor: pointer;
-	background-color: rgba(167, 167, 167, 0.74);
-}
-
-.board__favorite i {
-	font-size: 15px;
-}
-
-.list-container {
-	display: flex;
-}
-
-.list {
-	display: flex;
-	flex-direction: column;
-	padding: 10px;
-	margin-right: 10px;
-	width: 275px;
-	background-color: #ebecf0;
-	border-radius: 2px;
-}
-
-.list__title {
-	font-size: 1.4rem;
-	font-weight: bold;
-	margin-bottom: 10px;
-}
-
-.list__items__item {
-	display: flex;
-	align-items: center;
-	background-color: white;
-	width: 250px;
-	height: 30px;
-	margin-bottom: 8px;
-	border-radius: 2px;
-	box-shadow: 0px 1.3px rgb(182, 182, 182);
-}
-
-.list__items__item:hover {
-	background-color: rgb(240, 240, 240);
-	cursor: pointer;
-}
-
-.list__items__item__title {
-	margin-left: 8px;
-	font-size: 1.4rem;
-}
-
-.list__items__add {
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	margin-top: 10px;
-	width: 250px;
-	height: 30px;
-	border-radius: 3px;
-}
-
-.list__items__add:hover {
-	background-color: rgb(218, 218, 218);
-	cursor: pointer;
-}
-
-.list__items__add__text {
-	color: gray;
-	font-size: 1.3rem;
-}
-
-.list__items__add i {
-	color: gray;
-	font-size: 18px;
-}
-
-.add-list {
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	background-color: rgba(187, 187, 187, 0.74);
-	border-radius: 2px;
-	width: 250px;
-	height: 30px;
-}
-
-.add-list i {
-	color: gray;
-	font-size: 18px;
-}
-
-.add-list__text {
-	color: gray;
-	font-size: 1.3rem;
 }
 
 .card {
