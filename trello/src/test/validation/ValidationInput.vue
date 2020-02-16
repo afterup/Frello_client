e2402af740018bbf61b2c3291632034bcf1e637e
fora <afterup@naver.com>
2020-02-16 17:12:47 +0900

    Test: form validation

---
+++ trello/src/test/validation/ValidationInput.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <label :for="controlId"> {{ label }}</label>
+  <input :type="elementType"
+         :placeholder="placeholder"
+         :id="controlId"
+         :value="value"
+         @input="$emit('input', $event.target.value)"
+  >
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'controlId','label', 'elementType', 'placeholder', 'value', 'validation', 'invalid', 'touched'
+  ]
+}
+</script>
+
+<style>
+
+</style>
