66a80a5e9cd277a8cc131e57c31c6d328d4157e0
fora <uptohai@gmail.com>
2020-02-17 22:15:55 +0900

    Test: textarea resize test

---
+++ trello/src/test/resize/Page.vue
@@ -0,0 +1,22 @@
+<template>
+	<ResizeAuto>
+		<template v-slot:default="{ resize }">
+			<textarea
+				rows="2"
+				class="textarea"
+				placeholder="Write something for renderless component"
+				@input="resize"
+			></textarea>
+		</template>
+	</ResizeAuto>
+</template>
+
+<script>
+export default {
+	components: {
+		ResizeAuto: () => import('@/test/resize/ResizeAuto'),
+	},
+};
+</script>
+
+<style></style>
