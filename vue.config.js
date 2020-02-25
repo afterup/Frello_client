899d7d0d88ad0fb80436764fba344ba4f17407bb
fora <uptohai@gmail.com>
2020-02-25 13:33:25 +0900

    Fix: vue config chain webpack

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,21 @@ const path = require('path');
 
 module.exports = {
 	outputDir: path.resolve(__dirname, '../api/public'),
+	publicPath: '/',
+	chainWebpack: config => {
+		if (process.env.NODE_ENV === 'production') {
+			config.plugin('html').tap(args => {
+				args[0].minify.removeAttributeQuotes = false;
+				return args;
+			});
+		}
+	},
 	devServer: {
 		clientLogLevel: 'info',
 		port: 8080,
 		proxy: {
 			'/api': {
 				target: 'http://localhost:3000/api',
-				changeOrigin: true,
-				pathRewrite: {
-					'^/api': '',
-				},
 			},
 		},
 	},
